5483b9d811a889c34a1f984030170a794a3c0b40
daisukeyamaguchi329 <gaman55894@gmail.com>
2021-08-18 08:49:13 +0000

    Third Commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'eee4fcd5478876d8d4364156d076c7595d61644415003d7cec222f6116c9ce0cd52b1f265cd76e3404f1759b7b529292bf73cfd7c459ab8933cb3eddb60134ac'
