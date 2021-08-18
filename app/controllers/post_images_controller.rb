0e6f16ad5cd584e4af3dbe56623acc460b5f58e6
daisukeyamaguchi329 <gaman55894@gmail.com>
2021-08-18 14:30:55 +0000

    Sixth Commit

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -3,12 +3,16 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new
   end
 
- 
+
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path  end
+    if @post_image.save
+     redirect_to post_images_path
+    else
+      render :new
+    end
+  end
 
   def index
     @post_images = PostImage.page(params[:page]).reverse_order
@@ -25,11 +29,11 @@ class PostImagesController < ApplicationController
     redirect_to post_images_path
   end
 
- 
+
   private
 
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
+  
 end
-
